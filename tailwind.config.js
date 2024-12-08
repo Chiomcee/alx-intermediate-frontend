7a129ea45c19b32d8f8bdb22cdf82687516f66e6
Chioma Igwe <cigwe072013@gmail.com>
2024-12-08 23:43:25 +0100

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],  // Scans files for Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
